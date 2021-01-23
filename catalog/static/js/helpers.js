108ff53f8fa9e329a076f836940a53f8cb7736c7
Shahar Gino <danitgino@yahoo.com>
2021-01-24 00:01:47 +0200

    continous timeline enhancement - align with dev b546ba79

--- catalog/static/js/helpers.js
+++ catalog/static/js/helpers.js
@@ -19,6 +19,7 @@ function getColorByIndex(indexString) {
 }
 
 function get_seconds(e) {
+    if(!e) { return 0; }
     const t = e.split(":");
     return 60 * +t[0] * 60 + 60 * +t[1] + +t[2];
 }
@@ -29,6 +30,9 @@ function radians_to_degrees(radians) {
 }
 
 function secondsToHms(e) {
+    if(!e) {
+        return "00:00:00";
+    }
     e = Number(e);
     const t = Math.floor(e / 3600),
         a = Math.floor((e % 3600) / 60),
@@ -37,6 +41,9 @@ function secondsToHms(e) {
 }
 
 function secToHours (str) {
+    if(!str) {
+        return "00:00:00"
+    }
     const sec_num = parseInt(str, 10); // don't forget the second param
     let hours   = Math.floor(sec_num / 3600);
     let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
@@ -49,6 +56,9 @@ function secToHours (str) {
 }
 
 function hmsToSecondsOnly(str) {
+    if(!str) {
+        return 0;
+    }
     var p = str.split(':'),
         s = 0, m = 1;
 
@@ -60,8 +70,22 @@ function hmsToSecondsOnly(str) {
     return s;
 }
 
+function getStorageGpxState() {
+    const gpx = JSON.parse(localStorage.getItem('GPXState'));
+    if(!gpx) {
+        localStorage.setItem("GPXState", JSON.stringify(null));
+    }
+    return JSON.parse(localStorage.getItem('GPXState'));
+}
+
+function updateStorageGpxState(objProp) {
+    const currentState = getStorageGpxState() || {};
+    const newState = { ...currentState, ...objProp };
+    localStorage.setItem("GPXState", JSON.stringify(newState));
+}
+
 function getStorageState() {
-    return JSON.parse(localStorage.getItem("currentState"));
+    return JSON.parse(localStorage.getItem('currentState'));
 }
 
 function updateStorage(objProp) {
@@ -72,4 +96,47 @@ function updateStorage(objProp) {
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// ======== GPX localstorage cache ========== //
+async function cacheUpdate(ctx, func) {
+    let gpxCached = getStorageGpxState()?.gpxData
+    let newGpxCache = {};
+    const isGpxPresentInCache = getStorageGpxState()?.hasOwnProperty('gpxData');
+    if (!isGpxPresentInCache) {
+        console.log('Call for retrieving GPX data from server')
+        gpxData = await func.call(ctx);
+        if (trainSection !== 'None') {
+            newGpxCache = {
+                [trainingID]: {
+                    [trainSection]: gpxData
+                }
+            }
+        }
+        updateStorageGpxState({gpxData: newGpxCache})
+    } else {
+        if (trainingID && trainSection) {
+            const isID = getStorageGpxState().gpxData.hasOwnProperty(trainingID);
+            if (isID && trainingID !== 'None') {
+                const isSelId = isID && getStorageGpxState().gpxData[trainingID].hasOwnProperty(trainSection);
+                if (!isSelId && trainSection !== 'None') {
+                    console.debug(`New Training ID: ${trainingID}, Trainee ID: ${trainSection} has retrieved from SERVER`);
+                    gpxData = await func.call(ctx);
+                    newGpxCache[trainingID] = {...gpxCached[trainingID], ...{[trainSection]: gpxData}}
+                } else {
+                    console.debug(`This Trainee ID: ${trainSection} from Train ID: ${trainingID} has retrieved from CACHE`);
+                    gpxData = gpxCached[trainingID][trainSection];
+                }
+            } else {
+                if (trainSection !== 'None' && trainingID !== 'None') {
+                    console.debug(`No training ID ${trainingID} in the cache, so retrieve new data from SERVER`);
+                    gpxData = await func.call(ctx);
+                    newGpxCache[trainingID] = {
+                        [trainSection]: gpxData
+                    }
+                }
+            }
+            updateStorageGpxState({gpxData: {...gpxCached, ...newGpxCache}})
+        }
+    }
 }
