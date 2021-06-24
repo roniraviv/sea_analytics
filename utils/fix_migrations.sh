933081ac93f6b73798260009633e1a636db6631c
Shahar Gino <sgino@apple.com>
2021-06-24 09:50:01 +0300

    code_update and fix_migrations update, aligned with dev f1ebae5

--- utils/fix_migrations.sh
+++ utils/fix_migrations.sh
@@ -9,18 +9,18 @@ fake_en=${1:-false}
 python manage.py showmigrations
 
 if [ "${fake_en}" = true ]; then
-    python manage.py migrate --fake catalog
+    python manage.py migrate --fake
 fi
 
 # Migrate (if required):
-python manage.py migrate catalog --noinput
+python manage.py migrate --noinput
 
 # Create additional migrations (if required):
-python manage.py makemigrations catalog --noinput
-python manage.py makemigrations catalog --merge --noinput
+python manage.py makemigrations --noinput
+python manage.py makemigrations --merge --noinput
 
 # Migrate (if required):
-python manage.py migrate catalog
+python manage.py migrate
 
 # Show Migrations (after):
 python manage.py showmigrations
