3a8c0aaf8b840497023831490b85fb9395a8e378
Shahar Gino <danitgino@yahoo.com>
2021-11-09 09:56:09 +0200

    training appendices handling, adding option to delete existing objects, aligned with dev 29e4201

--- catalog/static/js/fileUpload.js
+++ catalog/static/js/fileUpload.js
@@ -12,7 +12,7 @@ const showWholeTableTr = () => {
   $("#dialog_table_header").show()
 }
 
-const fillRowWithData = (training_appendices_arr) => {
+const fillRowWithData = (training_id, training_appendices_arr, del_url) => {
   for (var k in training_appendices_arr) {
     let index = k;
     const filenamePart = training_appendices_arr[k].split(",")[0];
@@ -22,7 +22,9 @@ const fillRowWithData = (training_appendices_arr) => {
     const row = `<tr>
                      <td>${index}</td>
                      <td><a href="${filenamePart}" target="_blank" class="file_link">${filename}</a></td>
-                      <td>${description}</td></tr>
+                     <td>${description}</td>
+                     <td><a href="#" onclick='delete_appendix("${training_id}", ${k}, "${del_url}");'>X</a></td>
+                 </tr>
                  `;
     $(" #dialog_table_tr").append(row);
   }
@@ -52,6 +54,7 @@ const defineDialog = ({trainingId, url}) => {
         text: "Ok",
         id: "ok_button",
         click: function () {
+
           const formData = new FormData();
           var csrftoken = $("[name=csrfmiddlewaretoken]").val();
           formData.append('file', $("#file")[0].files[0]);
@@ -67,12 +70,11 @@ const defineDialog = ({trainingId, url}) => {
             processData: false,
             data: formData,
             success: () => {
-              alert('Data Submitted Successfully!');
               $("#dialog_table").dialog("close");
               location.reload();
             },
             error: (e) => {
-              alert('Data Submission Error! Please Try Again Later! ');
+              swal("Training Appendices", 'Appendix Submission Failed! Please Try Again Later!', "error");
               $("#dialog_table").dialog("close");
             }
           });
@@ -89,7 +91,6 @@ const disableSubmitButton = () => {
 const showDialog = () => {
   $("#dialog_table").dialog("open");
 }
-
 
 const validateDialog = () => {
   $("#description").on('keyup', (e) => {
@@ -114,4 +115,14 @@ const changeButtonProps = {
   ,
   enabled: () => $("#ok_button").removeAttr('disabled')
     .css({'background': 'green', 'color': 'white'})
+}
+
+function delete_appendix(training_id, appendix_id, del_url) {
+  $.ajax({
+      url: del_url,
+      data: { appendix_id: appendix_id,
+              training_id: training_id },
+      success: function() { location.reload(); },
+      error: function() { swal("Training Appendices", 'Appendix Removal Error! Please Try Again Later!', "error"); },
+  });
 }
